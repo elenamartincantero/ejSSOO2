ac341e062241561072233eab587e98b4239c8b01
elenamartincantero <elena.martin19@alu.uclm.es>
2022-03-14 19:45:53 +0100

    retos 5 y 5.1

---
+++ Reto_5.1/reto_5.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+
+class Inc{
+    public:
+        int x;
+        Inc(int x){
+            this->x = x;
+        }
+        void incrementar(){
+            this->x++;
+        }
+        void operator () () {
+            incrementar();
+            std::cout << "Desde el hilo: "<< x << std::endl;
+        }
+};
+
+int main(){
+    int x = 0;
+    Inc c(x);
+    std::cout << "Desde el main: "<< c.x << std::endl;
+    std::thread hilo(std::ref(c));
+    hilo.join();
+    std::cout << "Desde el main: "<< c.x << std::endl;
+}
